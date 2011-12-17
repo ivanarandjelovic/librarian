c9430d3d7ddcebd6b078c891a255c7b86925ed62
ivan_arandjelovic <ivan.arandjelovic@gmail.com>
2011-12-17 21:48:29 +0100

    Added some more tests (dummy ones)

---
+++ test/unit/org/aivan/librarian/Dummy2UnitTest.java
@@ -0,0 +1,14 @@
+package org.aivan.librarian;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Dummy2UnitTest {
+
+	@Test
+	public void test() {
+		assertTrue("All is well", true);
+	}
+
+}
---
+++ test/unit/org/aivan/librarian/DummyUnitTest.java
@@ -0,0 +1,14 @@
+package org.aivan.librarian;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DummyUnitTest {
+
+	@Test
+	public void test() {
+		assertTrue("All is well", true);
+	}
+
+}
