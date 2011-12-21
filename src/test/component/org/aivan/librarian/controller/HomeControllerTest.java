b489e17f15a8c461fd415cfbd171c7c8aba7ffc7
ivan_arandjelovic <ivan.arandjelovic@gmail.com>
2011-12-21 22:03:40 +0100

    Controller test finally working.

--- src/test/component/org/aivan/librarian/controller/HomeControllerTest.java
+++ src/test/component/org/aivan/librarian/controller/HomeControllerTest.java
@@ -7,19 +7,34 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.annotation.AnnotationMethodHandlerAdapter;
+
+import static org.springframework.test.web.ModelAndViewAssert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/spring/root-context.xml", "/spring/appServlet/servlet-context.xml" } )
+@ContextConfiguration(locations = { "/spring/root-context.xml", "/spring/appServlet/servlet-context.xml" })
 public class HomeControllerTest extends AbstractJUnit4SpringContextTests {
 
+	private MockHttpServletRequest request;
+    private MockHttpServletResponse response;
+    private HandlerAdapter handlerAdapter;
 	@Autowired
 	private HomeController homeController;
+
 	
 	@Before
 	public void setUp() throws Exception {
+		request = new MockHttpServletRequest();
+		response = new MockHttpServletResponse();
+		handlerAdapter = new AnnotationMethodHandlerAdapter();
 	}
 
 	@After
@@ -31,5 +46,17 @@ public class HomeControllerTest extends AbstractJUnit4SpringContextTests {
 		assertNotNull("Controller not present", homeController);
 	}
 
-	// See: http://blog.daum.net/kte472ca/8
+	@Test
+	public void testHome() throws Exception {
+
+		request.setRequestURI("/");
+		request.setMethod("GET");
+	       final ModelAndView mav = handlerAdapter.handle(request, response, 
+	           homeController);
+	       assertViewName(mav, "home");
+	       assertModelAttributeAvailable(mav, "books");
+	       assertModelAttributeAvailable(mav, "serverTime");
+
+	}
+
 }
