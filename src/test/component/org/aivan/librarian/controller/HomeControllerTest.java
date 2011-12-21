56bbe146f80e728f4ac42f98a0c2105ff0a07326
ivan_arandjelovic <ivan.arandjelovic@gmail.com>
2011-12-21 22:09:31 +0100

    More checks added

--- src/test/component/org/aivan/librarian/controller/HomeControllerTest.java
+++ src/test/component/org/aivan/librarian/controller/HomeControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ContextConfiguration;
