e383245427b14c0f0c2dd61515f83f02083c1294
ivan_arandjelovic <ivan.arandjelovic@gmail.com>
2011-12-25 01:08:26 +0100

    Trying with DB unit task ... but not working yet

--- src/test/functional/org/aivan/librarian/db_unit/SetupDBTestDbUnit.java
+++ src/test/functional/org/aivan/librarian/db_unit/SetupDBTestDbUnit.java
@@ -11,6 +11,8 @@ import org.dbunit.dataset.IDataSet;
 import org.dbunit.dataset.xml.XmlDataSet;
 import org.junit.Test;
 
+import com.mysql.jdbc.Connection;
+
 /**
  * "By default, Dbunit performs a CLEAN_INSERT operation before executing each test and performs no cleanup operation afterward."
  * so, we use this as a test that needs to be run before all other tests.
@@ -20,16 +22,21 @@ import org.junit.Test;
  */
 public class SetupDBTestDbUnit extends DBTestCase {
 
-	public SetupDBTestDbUnit(String name) throws FileNotFoundException, IOException {
+	public SetupDBTestDbUnit(String name) throws Exception {
 		super(name);
 		Properties props = new Properties();
 		String dbConfiguration = System.getProperty("db.configuration","dev");
 		props.load(new FileInputStream("config/"+dbConfiguration+"/db.properties"));
+
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_DRIVER_CLASS, props.getProperty("db.driver"));
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_CONNECTION_URL, props.getProperty("db.base.url")+"/"+props.getProperty("db.name"));
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_USERNAME, props.getProperty("db.user"));
 		System.setProperty(PropertiesBasedJdbcDatabaseTester.DBUNIT_PASSWORD, props.getProperty("db.password"));
-	}
+
+		String id = "http://www.dbunit.org/properties/datatypeFactory"; 
+		this.getConnection().getConfig().setProperty(id, "org.dbunit.ext.mysql.MySqlDataTypeFactory");
+		
+}
 
 	@Override
 	protected IDataSet getDataSet() throws Exception {
