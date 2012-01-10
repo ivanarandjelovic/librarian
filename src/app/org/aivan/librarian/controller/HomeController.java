402b1def2f6f33e655efc2e784fe8e8fef8af9c8
ivan_arandjelovic <ivan.arandjelovic@gmail.com>
2012-01-10 22:00:36 +0100

    Removed unused code.

--- src/app/org/aivan/librarian/controller/HomeController.java
+++ src/app/org/aivan/librarian/controller/HomeController.java
@@ -6,7 +6,6 @@ import java.util.Locale;
 
 
 import org.aivan.librarian.service.BookService;
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,8 +17,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = Logger.getLogger(HomeController.class);;
 	
 	public static final String URL_ROOT = "/";
 	public static final String URL_HOME = "/home";
@@ -42,15 +39,12 @@ public class HomeController {
 	 */
 	@RequestMapping(value = {URL_ROOT, URL_HOME}, method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! the client locale is "+ locale.toString());
-		
 		Date date = new Date();
+
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
 		String formattedDate = dateFormat.format(date);
 		
 		model.addAttribute(ATTR_SERVER_TIME, formattedDate );
-		
 		model.addAttribute(ATTR_BOOKS, bookService.getAllBooks());
 		
 		return VIEW_HOME;
