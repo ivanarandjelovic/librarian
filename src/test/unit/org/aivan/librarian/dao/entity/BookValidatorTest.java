dbce84b783b8f13634f7832493cdbc1891f45998
ivan_arandjelovic <ivan.arandjelovic@gmail.com>
2012-01-10 15:25:14 +0100

    Added unit tests for BookValidator class

---
+++ src/test/unit/org/aivan/librarian/dao/entity/BookValidatorTest.java
@@ -0,0 +1,54 @@
+package org.aivan.librarian.dao.entity;
+
+import static org.junit.Assert.*;
+
+import org.aivan.librarian.testdata.UnitTestData;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.validation.BindException;
+
+public class BookValidatorTest {
+
+	BookValidator bookValidator;
+	
+	@Before
+	public void setUp() throws Exception {
+		bookValidator = new BookValidator();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSupportedClass() {
+		assertTrue("Book class should be supported",bookValidator.supports(Book.class));
+	}
+
+	@Test
+	public void testValidationEmptyTitle() {
+		Book book = new Book();
+		book.setTitle("");
+		
+		BindException binding = new BindException(book,"book");
+		
+		bookValidator.validate(book, binding);
+		
+		assertTrue("Binding should contain error", binding.hasErrors());
+	}
+
+	@Test
+	public void testValidationRegaularCase() {
+
+		Book book = new Book();
+		book.setTitle("non-empty");
+		
+		BindException binding = new BindException(book,"book");
+		
+		bookValidator.validate(book, binding);
+		
+		assertFalse("Binding should not contain error(s)", binding.hasErrors());
+	}
+
+}
