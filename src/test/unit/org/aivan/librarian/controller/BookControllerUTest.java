9c005f6314cf1440ae2fe51e550a6366ffa4e967
ivan_arandjelovic <ivan.arandjelovic@gmail.com>
2012-03-10 17:52:18 +0100

    added unit tests

--- src/test/unit/org/aivan/librarian/controller/BookControllerUTest.java
+++ src/test/unit/org/aivan/librarian/controller/BookControllerUTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.Locale;
 
-import org.aivan.librarian.dao.BookDao;
+import org.aivan.librarian.dao.BookDaoMockImpl;
 import org.aivan.librarian.dao.entity.Book;
 import org.aivan.librarian.service.BookService;
 import org.aivan.librarian.testdata.UnitTestData;
@@ -17,88 +17,109 @@ import org.springframework.validation.BindException;
 
 public class BookControllerUTest {
 
-	BookController bookController;
+  BookController bookController;
+  BookDaoMockImpl bookDaoMock;
 
-	Locale locale;
+  Locale locale;
 
-	@Before
-	public void setUp() throws Exception {
-		BookDao bookDaoMock = UnitTestData.createBookDaoMock();
+  @Before
+  public void setUp() throws Exception {
+    bookDaoMock = UnitTestData.createBookDaoMock();
 
-		BookService bookService = new BookService(bookDaoMock);
+    BookService bookService = new BookService(bookDaoMock);
 
-		bookController = new BookController(bookService);
+    bookController = new BookController(bookService);
 
-		locale = new Locale("en");
-	}
+    locale = new Locale("en");
+  }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+  @After
+  public void tearDown() throws Exception {
+  }
 
-	@Test
-	public void testOpenNewBook() {
-		Book book = new Book();
-		BindException binding = new BindException(book, "book");
+  @Test
+  public void testOpenNewBook() {
+    Book book = new Book();
+    BindException binding = new BindException(book, "book");
 
-		String view = bookController.openNewBook(book, binding);
+    String view = bookController.openNewBook(book, binding);
 
-		assertEquals("new book opening page/view  is wrong", "new_book", view);
-	}
+    assertEquals("new book opening page/view  is wrong", "new_book", view);
+  }
 
-	@Test
-	public void testCreateNewBook() {
-		Book book = UnitTestData.testBooks[0];
-		BindException binding = new BindException(book, "book");
+  @Test
+  public void testCreateNewBook() {
+    Book book = UnitTestData.testBooks[0];
+    BindException binding = new BindException(book, "book");
 
-		String view = bookController.createNewBook(book, binding);
+    String view = bookController.createNewBook(book, binding);
 
-		assertFalse("Binding should have no errors", binding.hasErrors());
-		assertEquals("new book creation view is wrong", "new_book_created", view);
-		assertTrue("book id is zero or null", book.getId() != 0 && book.getId() != null);
-	}
+    assertFalse("Binding should have no errors", binding.hasErrors());
+    assertEquals("new book creation view is wrong", "new_book_created", view);
+    assertTrue("book id is zero or null", book.getId() != 0 && book.getId() != null);
+  }
 
-	@Test
-	public void testCreateNewBookEmptyTitle() {
-		Book book = UnitTestData.testBooks[0];
-		book.setId(null);
-		book.setTitle("");
+  @Test
+  public void testCreateNewBookEmptyTitle() {
+    Book book = UnitTestData.testBooks[0];
+    book.setId(null);
+    book.setTitle("");
 
-		BindException binding = new BindException(book, "book");
+    BindException binding = new BindException(book, "book");
 
-		String view = bookController.createNewBook(book, binding);
+    String view = bookController.createNewBook(book, binding);
 
-		assertTrue("Binding should have error(s)", binding.hasErrors());
-		assertEquals("new book creation view is wrong after error", "new_book", view);
-		assertTrue("book id should be null", book.getId() == null);
-	}
+    assertTrue("Binding should have error(s)", binding.hasErrors());
+    assertEquals("new book creation view is wrong after error", "new_book", view);
+    assertTrue("book id should be null", book.getId() == null);
+  }
 
-	@Test
-	public void testEditBook() {
-		Model model = new ExtendedModelMap();
-		String view = bookController.editBook(UnitTestData.testBooks[0].getId(), model);
-		assertEquals("edit book opening page/view  is wrong", "edit_book", view);
-	}
+  @Test
+  public void testEditBook() {
+    Model model = new ExtendedModelMap();
+    String view = bookController.editBook(UnitTestData.testBooks[0].getId(), model);
+    assertEquals("edit book opening page/view  is wrong", "edit_book", view);
+  }
 
-	@Test
-	public void testEditBookDone() {
+  @Test
+  public void testEditBookDone() {
 
-		String newTestBookTitle = "test book title " + System.currentTimeMillis();
-		UnitTestData.testBooks[0].setTitle(newTestBookTitle);
-		BindException binding = new BindException(UnitTestData.testBooks[0], "book");
-		String view = bookController.saveBook(UnitTestData.testBooks[0], binding);
-		assertFalse("Binding should have no errors", binding.hasErrors());
-		assertEquals("edit book done view is wrong", "edit_book_done", view);
-	}
+    String newTestBookTitle = "test book title " + System.currentTimeMillis();
+    UnitTestData.testBooks[0].setTitle(newTestBookTitle);
+    BindException binding = new BindException(UnitTestData.testBooks[0], "book");
+    String view = bookController.saveBook(UnitTestData.testBooks[0], binding);
+    assertFalse("Binding should have no errors", binding.hasErrors());
+    assertEquals("edit book done view is wrong", "edit_book_done", view);
+  }
 
-	@Test
-	public void testEditBookEmptyTitleDone() {
-		String newTestBookTitle = "";
-		UnitTestData.testBooks[0].setTitle(newTestBookTitle);
-		BindException binding = new BindException(UnitTestData.testBooks[0], "book");
-		String view = bookController.saveBook(UnitTestData.testBooks[0], binding);
-		assertTrue("Binding should have error!", binding.hasErrors());
-		assertEquals("edit book with error should return view for editing again", "edit_book", view);
-	}
+  @Test
+  public void testEditBookEmptyTitleDone() {
+    String newTestBookTitle = "";
+    UnitTestData.testBooks[0].setTitle(newTestBookTitle);
+    BindException binding = new BindException(UnitTestData.testBooks[0], "book");
+    String view = bookController.saveBook(UnitTestData.testBooks[0], binding);
+    assertTrue("Binding should have error!", binding.hasErrors());
+    assertEquals("edit book with error should return view for editing again", "edit_book", view);
+  }
 
+  @Test
+  public void testDeleteBook() {
+
+    String newTestBookTitle = "test book title " + System.currentTimeMillis();
+    UnitTestData.testBooks[0].setTitle(newTestBookTitle);
+    Model model = new ExtendedModelMap();
+    String view = bookController.deleteBook(UnitTestData.testBooks[0].getId(), model);
+    assertEquals("delete book view is wrong", "delete_book", view);
+  }
+
+  @Test
+   public void testDeleteBookDone() {
+
+     String newTestBookTitle = "test book title " + System.currentTimeMillis();
+     UnitTestData.testBooks[0].setTitle(newTestBookTitle);
+     Model model = new ExtendedModelMap();
+     String view = bookController.deleteBookConfirm(UnitTestData.testBooks[0].getId(), model);
+     assertNull("Book should have been deleted!", bookDaoMock.getBooks().get(UnitTestData.testBooks[0].getId()));
+     assertEquals("delete book view is wrong", "delete_book_done", view);
+   }
 }
--- src/test/unit/org/aivan/librarian/testdata/UnitTestData.java
+++ src/test/unit/org/aivan/librarian/testdata/UnitTestData.java
@@ -1,6 +1,5 @@
 package org.aivan.librarian.testdata;
 
-import org.aivan.librarian.dao.BookDao;
 import org.aivan.librarian.dao.BookDaoMockImpl;
 import org.aivan.librarian.dao.entity.Book;
 import org.aivan.librarian.dao.entity.BookMock;
@@ -10,8 +9,8 @@ public class UnitTestData {
 	public static Book[] testBooks = new Book[] { new BookMock(-1, "Book 1"), new BookMock(-2, "Book 2"),
 	new BookMock(-3, "Book 3"), new BookMock(-4, "Book 4"), new BookMock(-5, "Book 5") };
 
-	public static BookDao createBookDaoMock() {
-		BookDao bookDaoMock = new BookDaoMockImpl();
+	public static BookDaoMockImpl createBookDaoMock() {
+	  BookDaoMockImpl bookDaoMock = new BookDaoMockImpl();
 
 		for (Book book : UnitTestData.testBooks) {
 			bookDaoMock.createBook(book);
